fcc01a4d0483ade45b1f23a6907e20393cbf3fbf
Mohammed Nabeel <abdulmuminnabeelmohammed@gmail.com>
2025-08-18 13:56:03 +0100

    edits

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
+from gpiozero import Buzzer
+
+GPIO.setmode(GPIO.BCM)
 
 # --- GPIO PINS ---
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 2       # GPIO pin for DHT22 data
 LDR_PIN = 22      # GPIO pin connected to LDR digital output
 RELAY_PIN = 21    # GPIO pin connected to relay module
+Red_led = 3     # GPIO pin connected to White  Led
+Green_led = 4     # GPIO pin connected to Green Led
+buzzer = Buzzer(20)
 
 # --- GPIO SETUP ---
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 GPIO.setup(LDR_PIN, GPIO.IN)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.setup(Red_led, GPIO.OUT)
+GPIO.setup(Green_led, GPIO.OUT)
+
+
 
 print("Starting Smart Environment Monitor...")
 
 try:
     while True:
-        # --- Read DHT22 ---
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
         if humidity is not None and temperature is not None:
             print(f"Temp: {temperature:.1f}°C  Humidity: {humidity:.1f}%")
@@ -34,12 +41,15 @@ try:
 
         # --- Fan Control (Relay) ---
         if temperature is not None and temperature > 28:
-            GPIO.output(RELAY_PIN, GPIO.HIGH)  # Fan ON
             print("Fan ON (hot temperature)")
+            GPIO.output(RELAY_PIN, GPIO.HIGH)  # Relay ON
+            GPIO.output(Red_led, GPIO.HIGH)  # Red led On
+            GPIO.output(Green_led, GPIO.LOW)  # Green led On
         else:
-            GPIO.output(RELAY_PIN, GPIO.LOW)   # Fan OFF
             print("Fan OFF (cool temperature)")
-
+            GPIO.output(RELAY_PIN, GPIO.LOW)   # relay OFF
+            GPIO.output(Green_led, GPIO.HIGH)   # Green led  On
+            GPIO.output(Red_led, GPIO.LOW) # Red led OFF
         time.sleep(2)
 
 except KeyboardInterrupt:
