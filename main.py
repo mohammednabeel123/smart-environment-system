81ffae6ff56075fa267425ffb3daa4fa82594298
Mohammed Nabeel <abdulmuminnabeelmohammed@gmail.com>
2025-08-19 00:05:46 +0100

    edits

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ try:
         # --- Read LDR ---
         if GPIO.input(LDR_PIN) == 1:
             print("LDR: Dark")
-             GPIO.output(RED_LED, GPIO.LOW)       # Red LED OFF
-             GPIO.output(GREEN_LED, GPIO.HIGH)    # Green LED ON
+            GPIO.output(RED_LED, GPIO.LOW)       # Red LED OFF
+            GPIO.output(GREEN_LED, GPIO.HIGH)    # Green LED ON
         else:
             print("LDR: Light detected")
             GPIO.output(RED_LED, GPIO.HIGH)      # Red LED ON
@@ -54,7 +54,6 @@ try:
             print("Fan ON (Red LED + Relay + Buzzer)")
             GPIO.output(RELAY_PIN, GPIO.HIGH)    # Relay ON
             
-         
             buzzer.on()                          # Buzzer ON
         else:
             print("Fan OFF (Green LED)")
