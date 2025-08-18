ab53e84d18cc8eb077cc1bc4ce91213eb96c500c
Mohammed Nabeel <abdulmuminnabeelmohammed@gmail.com>
2025-08-19 00:43:34 +0100

    edits

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ try:
             GPIO.output(GREEN_LED, GPIO.HIGH)
 
         # --- Temperature relay/fan control ---
-        if temperature <= FAN_TEMP_THRESHOLD:
+        if temperature >= FAN_TEMP_THRESHOLD:
             print("Fan ON (Relay + Buzzer)")
             print(f"Temperature raw value: {repr(temperature)}, type: {type(temperature)}")
             GPIO.output(RELAY_PIN, GPIO.HIGH)
